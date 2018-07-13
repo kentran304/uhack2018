1ff61846cdfc3a04a165cfe421cd4d3f16f9beb1
kentran304 <tranmr@gmail.com>
2018-07-13 19:15:38 +1000

    Creating models and add to Admin site

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Restaurant, Customer, Meal, Order, OrderDetail
+
+admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Meal)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
