70daa7395921ccd41ed330eefb88788640211e58
kentran304 <tranmr@gmail.com>
2018-07-13 20:15:09 +1000

    Adding index.html and /restaurant router

--- leftoven/urls.py
+++ leftoven/urls.py
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp import views
+
+# Import static and setting
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.home, name='home'),
+    path('restaurant/', views.restaurant_home, name='restaurant-home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def home(request):
+    # return redirect(restaurant_home)
+    return render(request, 'index.html', {})
+
+@login_required(login_url='/restaurant/sign-in')
+def restaurant_home(request):
+    return render(request, 'restaurant/base.html', {})
