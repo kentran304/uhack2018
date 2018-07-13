4db5648cc06813c636354fee26ed7518614a8875
kentran304 <tranmr@gmail.com>
2018-07-13 20:36:57 +1000

    Adding views.restaurant controllers

--- leftoven/urls.py
+++ leftoven/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from webapp import views
+from django.contrib.auth import views as auth_views
+
 
 # Import static and setting
 from django.conf.urls.static import static
@@ -26,6 +28,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('restaurant/', views.restaurant_home, name='restaurant-home'),
+    path('restaurant/sign-in', auth_views.login, {'template_name': 'restaurant/sign_in.html'}, name='restaurant-sign-in'),
+    path('restaurant/sign-out', auth_views.logout, {'next_page': '/'}, name='restaurant-sign-out'),
+    path('restaurant/sign-up', views.restaurant_sign_up, name='restaurant-sign-up'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- webapp/views.py
+++ webapp/views.py
@@ -9,3 +9,26 @@ def home(request):
 @login_required(login_url='/restaurant/sign-in')
 def restaurant_home(request):
     return render(request, 'restaurant/base.html', {})
+
+@login_required(login_url='/restaurant/sign-in')
+def restaurant_account(request):
+    return render(request, 'restaurant/account.html', {})
+
+@login_required(login_url='/restaurant/sign-in')
+def restaurant_meal(request):
+    return render(request, 'restaurant/meal.html', {})
+
+@login_required(login_url='/restaurant/sign-in')
+def restaurant_order(request):
+    return render(request, 'restaurant/order.html', {})
+
+@login_required(login_url='/restaurant/sign-in')
+def restaurant_report(request):
+    return render(request, 'restaurant/report.html', {})
+
+def restaurant_sign_up(request):
+    return render(request, 'restaurant/sign_up.html', {
+        "user_form": user_form,
+        "restaurant_form": restaurant_form
+    })
+
